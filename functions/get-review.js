590c53afe452a13139091c1681772c106184a56a
Sergio Pinto <serrique@icloud.com>
2023-11-16 18:18:21 -0500

    week 3 - partial
    Signed-off-by: Sergio Pinto <serrique@icloud.com>

---
+++ functions/get-review.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const Cloudant = require('@cloudant/cloudant');
+
+// Initialize Cloudant connection with IAM authentication
+async function dbCloudantConnect() {
+    try {
+        const cloudant = Cloudant({
+            plugins: { iamauth: { iamApiKey: 'xK2-SbF9cArNQOnJdufIAKbTGoF6U2U2RM8GCS1RTwOw' } }, // Replace with your IAM API key
+            url: 'https://e6e4172f-40ce-4983-b95e-dafcdf47019e-bluemix.cloudantnosqldb.appdomain.cloud', // Replace with your Cloudant URL
+        });
+
+        const db = cloudant.use('reviews');
+        console.info('Connect success! Connected to DB');
+        return db;
+    } catch (err) {
+        console.error('Connect failure: ' + err.message + ' for Cloudant DB');
+        throw err;
+    }
+}
+
+let db;
+
+(async () => {
+    db = await dbCloudantConnect();
+})();
+
+app.use(express.json());
+
+// Define a route to get all dealerships with optional state and ID filters
+app.get('/reviews/get', (req, res) => {
+    const { state, id } = req.query;
+
+    // Create a selector object based on query parameters
+    const selector = {};
+    if (state) {
+        selector.state = state;
+    }
+    
+    if (id) {
+        selector.id = parseInt(id); // Filter by "id" with a value of 1
+    }
+
+    const queryOptions = {
+        selector,
+        limit: 10, // Limit the number of documents returned to 10
+    };
+
+    db.find(queryOptions, (err, body) => {
+        if (err) {
+            console.error('Error fetching reviews:', err);
+            res.status(500).json({ error: 'An error occurred while fetching reviews.' });
+        } else {
+            const reviews = body.docs;
+            res.json(reviews);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
